ee460a2a34906ba16651f90fa8a2aa64d6842d55
nicolas_cossio <nicolas_cossio@PCNico>
2020-12-11 11:05:55 +0100

    terminado lab 6

--- src/individual6/Suma.java
+++ src/individual6/Suma.java
@@ -2,39 +2,39 @@ package individual6;
 
 import es.upm.aedlib.graph.Edge;
 import es.upm.aedlib.graph.Vertex;
-
-import java.util.Iterator;
-
-import es.upm.aedlib.Position;
 import es.upm.aedlib.graph.DirectedAdjacencyListGraph;
 import es.upm.aedlib.graph.DirectedGraph;
 import es.upm.aedlib.map.Map;
-import es.upm.aedlib.set.PositionListSet;
+import es.upm.aedlib.set.HashTableMapSet;
 import es.upm.aedlib.set.Set;
 import es.upm.aedlib.map.HashTableMap;
 
 public class Suma {
 
-	private static <E> int sumReachable(Vertex<Integer> vert, int intv, DirectedGraph<Integer, E> graph, Set<Position<Integer>> visitados) {
-		if(!visitados.contains(vert)) {
-			intv+= vert.element();
-			visitados.add(vert);
+	private static <E> int sumReachable(Vertex<Integer> vert, DirectedGraph<Integer, E> graph, Set<Vertex<Integer>>visitados) {
+		int suma = vert.element();
+		System.out.println("Vertice: "+vert);
+		for(Edge<E> outEdge : graph.outgoingEdges(vert)) {
+			Vertex<Integer> nextvert=graph.endVertex(outEdge);
+			System.out.println(visitados.contains(nextvert));
+			if (!visitados.contains(nextvert)) {
+				visitados.add(nextvert);
+				suma +=sumReachable(nextvert, graph, visitados);
+			}
 		}
-		for( Edge<E> outEdge : graph.outgoingEdges(vert)) {
-			sumReachable(graph.endVertex(outEdge), intv, graph, visitados);
-		}
-		return intv;
+		System.out.println("suma final=" +suma);
+		return suma;
 	}
 
 	public static <E> Map<Vertex<Integer>, Integer> sumVertices(DirectedGraph<Integer, E> graph) {
 		Map<Vertex<Integer>, Integer> map = new HashTableMap<Vertex<Integer>, Integer>();
-		DirectedGraph<Integer, E> grap = new DirectedAdjacencyListGraph<Integer, E>();
 		System.out.println(graph.toDot());
 		if (!graph.isEmpty()) {
 			for (Vertex<Integer> vertex : graph.vertices()) { 
-				Set<Position<Integer>> visitados = new PositionListSet<Position<Integer>>();
-				int sum = sumReachable(vertex, 0, graph, visitados);
-				map.put(vertex, sum);
+				Set<Vertex<Integer>> visitados = new HashTableMapSet<Vertex<Integer>>();
+				visitados.add(vertex);
+				System.out.println("\n nuevoVert: "+vertex );
+				map.put(vertex,sumReachable(vertex, graph, visitados));
 			}
 		}
 		return map;
